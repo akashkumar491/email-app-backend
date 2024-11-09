a33ff60ae1fdf5cdf5b7dade2c0960859fbb4962
akashkumar491 <visitak7@gmail.com>
2024-11-09 14:08:59 +0530

    Update schema

--- models/Email.js
+++ models/Email.js
@@ -4,6 +4,8 @@ const emailSchema = new mongoose.Schema({
   subject: String,
   sender: String,
   body: String,
+  isRead: Boolean,
+  isFavourite: Boolean,
   receivedAt: { type: Date, default: Date.now },
 });
 
--- routes/email.js
+++ routes/email.js
@@ -52,7 +52,7 @@ router.put("/update/:id", async (req, res) => {
     const updatedEmail = await Email.findByIdAndUpdate(
       emailId,
       { $set: updatedData },
-      { new: true }
+      { new: true, runValidators: true }
     );
 
     if (!updatedEmail) {
